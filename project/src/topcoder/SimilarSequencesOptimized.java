2680469384fe329b916a2181da6b3ea9d7494f87
deviprasad742 <deviprasad742@gmail.com>
2013-08-19 12:00:04 +0530

    Commit from the new lappy (Xilinx)

--- project/src/topcoder/SimilarSequencesOptimized.java
+++ project/src/topcoder/SimilarSequencesOptimized.java
@@ -4,9 +4,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.ibm.disthub2.impl.gd.ARangeList;
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 /**
  * 
  * @author Prasad
